8591244782447eae10d1e6afeca5eb2e1cbe62b5
Hyihuiowwwii <87595941+Hyihuiowwwii@users.noreply.github.com>
2026-03-26 20:57:50 +0530

    Add function to generate dummy dataset results

---
+++ src/dataset_analysis.py
@@ -0,0 +1,10 @@
+def get_dummy_dataset_results():
+    return [
+        {"subject": "5-gt", "gt_hr": 77.3, "est_hr": 63.5, "error": 13.9, "accuracy": "82.1%"},
+        {"subject": "6-gt", "gt_hr": 82.6, "est_hr": 68.0, "error": 14.6, "accuracy": "82.3%"},
+        {"subject": "7-gt", "gt_hr": 94.7, "est_hr": 78.1, "error": 16.6, "accuracy": "82.5%"},
+        {"subject": "8-gt", "gt_hr": 68.1, "est_hr": 52.3, "error": 15.9, "accuracy": "76.7%"},
+        {"subject": "10-gt", "gt_hr": 72.4, "est_hr": 57.9, "error": 14.6, "accuracy": "79.9%"},
+        {"subject": "11-gt", "gt_hr": 77.7, "est_hr": 64.9, "error": 12.8, "accuracy": "83.5%"},
+        {"subject": "12-gt", "gt_hr": 94.6, "est_hr": 76.9, "error": 17.7, "accuracy": "81.3%"},
+    ]
